d4579348e047eb42b821d95783459e78c1e8b202
Jlyman2001 <lymanj@sonoma.edu>
2024-10-21 10:11:55 -0700

    Update comments on files

--- Wifi Sensor/src/ESPWiFi.h
+++ Wifi Sensor/src/ESPWiFi.h
@@ -1,4 +1,5 @@
 #pragma once
+//Include needed libraries
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
@@ -9,18 +10,25 @@
 #include "dhtnew.cpp"
 #include <ArduinoJson.h>
 
-
+//define better names for the IO pins
 #define LED_1_PIN LED_BUILTIN
 #define BUTTON_CAPTURE_EXT_PULL_UP D0
 #define TEMPERATURE_HUMIDITY_SENSOR D1
 
+//define the sensor object
 DHTNEW Sensor(TEMPERATURE_HUMIDITY_SENSOR);
 
+//define constant values needed for operation such
+//as the WiFi AP name and password
+//as well as the URLs needed to query
 const char* ssid = "Lyman Family";
 const char* password = "7072178282";
+
 String baseURL = "https://josh-lyman-ssu.com/Database-Assignment/insert_data.php";
 String timeURL = "https://timeapi.io/api/Time/current/zone?timeZone=";
 String Node_ID = "node-6";
+
+//global String to store timezone query
 String timezoneStr;
 
 using namespace std;
@@ -42,21 +50,27 @@ void connectToWiFi()
   Serial.println(ssid);
 
 
-  //wifi init
+  //begin trying to connect to specified network
   WiFi.begin(ssid,password);
+  //wait until connection is established
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(300);
     Serial.print(".");
   }
+
+  //print to Serial that connection was successful
   Serial.println();
   Serial.print("Successfully connected to ");
   Serial.println(ssid);
   Serial.println();
+
+  //turn on the LED to show that it is connected
   digitalWrite(LED_1_PIN, LOW);
 }
 
 void setTimeZone(){
+    //Prompt the user for an input with text on Serial
     Serial.println("Please select a timezone:");
     Serial.println("1) Eastern Time Zone (ET) - New York City, NY");
     Serial.println("2) Central Time Zone (CT) -Chicago, IL");
@@ -65,17 +79,18 @@ void setTimeZone(){
     Serial.println("5) Alaska Time Zone (AKT) - Anchorage, AK");
     Serial.println("6) Hawaii-Aleutian Time Zone (HAT) - Honolulu, HI");
     Serial.println("7) Atlantic Time Zone (AT) - San Juan, Puerto Rico (US territory, not part of the contiguous US).");
-
     Serial.println("You have 7 seconds to make a choice or PT is chosen automatically.");
 
+    //wait for user input
     delay(7000);
-
-    
 
     //this means that a value has been input
     if (Serial.available())
     {
+        //read input character
         char timezoneInt = Serial.read();
+
+        //depending on input, set timezoneStr to be string for query
         switch (timezoneInt)
         {
             case '1':
@@ -99,6 +114,7 @@ void setTimeZone(){
             case '7':
                 timezoneStr = "America/Puerto_Rico";
                 break;
+            //If input was invalid, select PT
             default:
                 Serial.println("Invalid option selected. Using default value of PT");
                 timezoneStr = "US/Pacific";
@@ -106,6 +122,7 @@ void setTimeZone(){
         }
         Serial.println("You have selected: " + timezoneStr);
     }
+    //if no input was given, choose PT
     else
     {
         Serial.println("No input received. Using default value of PT");
@@ -129,6 +146,8 @@ double captureHumidity()
 
 void uploadData(String timestamp, double temperature, double humidity, int recursionNumber)
 {
+    //Recursion check to allow for retrying transmission
+    //if it has failed 4 times, just give up
     if (recursionNumber > 3)
     {
         Serial.println("Error: Too many failed requests.");
@@ -138,47 +157,62 @@ void uploadData(String timestamp, double temperature, double humidity, int recur
 
 
 
-     String TargetURL;
+    String TargetURL;
+
+    //if there is a timestamp, add it with the rest of the parameters
 	if (timestamp != "")
 	{
-    TargetURL = baseURL + String("?node_name=") + String(Node_ID) + String("&time_received=") + String(timestamp) + String("&temperature=") + String(temperature) + String("&humidity=") + String(humidity);
-    //String TargetURL = baseURL  + TimeParameter + NodeParameter + TemperatureParameter + HumidityParameter;
+        //construct the target URL string with given parameters
+        TargetURL = baseURL + String("?node_name=") + String(Node_ID) + String("&time_received=") + String(timestamp) + String("&temperature=") + String(temperature) + String("&humidity=") + String(humidity);
 	}
 	else
+    //if no timestamp is given, omit the string for the parameter to let the server
+    //handle the time
 	{
 		//if no valid timestamp, omit and make server add
+        //construct the target URL string with given parameters
 		TargetURL = baseURL + String("?node_name=") + String(Node_ID) + String("&temperature=") + String(temperature) + String("&humidity=") + String(humidity);
     
 	}
 	
+    //ensure wifi is connected before trying to send
     if (WiFi.status() == WL_CONNECTED)
     {
-    WiFiClientSecure client;
-    client.setInsecure();
-    HTTPClient https;
+        //create wifi client and http client
+        WiFiClientSecure client;
+        client.setInsecure();
+        HTTPClient https;
 
-    Serial.println("Requesting: -->" + TargetURL);
-    Serial.println();
-    if (https.begin(client,TargetURL))
-    {
-        int httpCode = https.GET();
-        Serial.println("Response code <--: " + String(httpCode));
-        if (httpCode == -1)
+        //print to Serial for monitoring status
+        Serial.println("Requesting: -->" + TargetURL);
+        Serial.println();
+
+        //ensure that the client can begin the request
+        if (https.begin(client,TargetURL))
         {
-            Serial.println("Wifi appears to have been disconnected. Retrying.");
-            WiFi.disconnect();
-            connectToWiFi();
+            //fetch response code from GET query
+            int httpCode = https.GET();
+            //Print response code to Serial
+            Serial.println("Response code <--: " + String(httpCode));
+
+            //if the code is -1, it is a software issue on the ESP.
+            //Disconnect wifi, reconnect, and recursively try again to send
+            if (httpCode == -1)
+            {
+                Serial.println("Wifi appears to have been disconnected. Retrying.");
+                WiFi.disconnect();
+                connectToWiFi();
+                https.end();
+                
+                uploadData(timestamp, temperature, humidity, recursionNumber + 1);
+            }
             https.end();
             
-            uploadData(timestamp, temperature, humidity, recursionNumber + 1);
         }
-        https.end();
-        
-    }
-    else
-    {
-        Serial.printf("[HTTPS] Unable to connect\n");
-    }
+        else
+        {
+            Serial.printf("[HTTPS] Unable to connect\n");
+        }
 
     }
 
--- Wifi Sensor/src/main.cpp
+++ Wifi Sensor/src/main.cpp
@@ -8,19 +8,32 @@ void setup() {
   // put your setup code here, to run once:
 
   //set baud rate to 9600
-  Serial.begin(9600);             
+  Serial.begin(9600);         
+
+  //print whitespace before any data for readability
   Serial.println();
-  Serial.println(WiFi.macAddress());
+  //Serial.println(WiFi.macAddress());
+  
   //set button to be input (external pull-up)
   pinMode(BUTTON_CAPTURE_EXT_PULL_UP, INPUT);  
+  
+  //set pin mode for sensor input
   pinMode(TEMPERATURE_HUMIDITY_SENSOR, INPUT);
+  
+  //set pin mode for built in LED
   pinMode(LED_1_PIN, OUTPUT);
-  //LED is inverted logic, this turns it off
+  
+  //LED is inverted logic, this ensures it is off
   digitalWrite(LED_1_PIN, HIGH);
   
+  //wait a second 
   delay(1000);
 
+  //set the time zone with input from Serial
   setTimeZone();
+
+  //connect to the specified WiFi AP 
+  //as defined in the header
   connectToWiFi();
 
   
